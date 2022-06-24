48d69ba3d033e7217e940eb7772287533ac43a1d
Viral <viral@bettertrucks.com>
2022-06-24 10:24:15 +0530

    change url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'https://geospree-services-stage.herokuapp.com/api',
-      target: 'http://localhost:4000/api',
+      target: 'https://geospree-services-stage.herokuapp.com/api',
+      // target: 'http://localhost:4000/api',
       changeOrigin: true,
       pathRewrite: { '^/api': '' }
     })
